b01ba0301a216b8caf461ccbb23e670c6bcbebd4
Baraah Arebi <baraah.areebi@cet.edu.ly>
2026-05-30 02:44:51 +0200

    ةة

--- Reports/StockInventoryStatus.cs
+++ Reports/StockInventoryStatus.cs
@@ -103,6 +103,14 @@ namespace InventoryManagementSystem.Reports {
                 return this.ReportDefinition.Sections[6];
             }
         }
+        
+        [Browsable(false)]
+        [DesignerSerializationVisibilityAttribute(System.ComponentModel.DesignerSerializationVisibility.Hidden)]
+        public CrystalDecisions.Shared.IParameterField Parameter_CurrentUser {
+            get {
+                return this.DataDefinition.ParameterFields[0];
+            }
+        }
     }
     
     [System.Drawing.ToolboxBitmapAttribute(typeof(CrystalDecisions.Shared.ExportOptions), "report.bmp")]
